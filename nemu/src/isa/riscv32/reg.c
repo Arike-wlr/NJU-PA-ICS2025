7f4dcffe48d2a8d7347f2a1661d130fae4778cdd
Arike <1637928975@qq.com>
2025-08-28 23:26:34 +0800

    把reg的代码写到x86里面去了啊啊啊啊啊啊我真服了，改回来了（）

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -24,8 +24,22 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
+   for(int i=0;i<32;i++){
+    printf("%-8s%-#20x%-20d\n",regs[i],cpu.gpr[i],cpu.gpr[i]);
+  }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
+ if(strcmp("pc",s)==0){
+    *success=true;
+    return cpu.pc;
+  }
+  for(int i=0;i<32;i++){
+    if(strcmp(regs[i],s)==0){
+      *success = true;
+      return cpu.gpr[i];
+    }
+  }
+  *success = false;
   return 0;
 }

015ea2a9e1405ac1a9a5e10c68cf766064b7f968
tracer-ics2025 <tracer@njuics.org>
2025-10-19 22:21:59 +0800

    >  compile NEMU 241300009 王琳睿 Linux LAPTOP-CHR6Q9L1 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:21:57 up 4 min,  1 user,  load average: 0.16, 0.05, 0.01

--- abstract-machine/am/src/platform/nemu/include/nemu.h
+++ abstract-machine/am/src/platform/nemu/include/nemu.h
@@ -21,19 +21,20 @@
 #if defined(__ARCH_X86_NEMU)
 # define DEVICE_BASE 0x0
 #else
-# define DEVICE_BASE 0xa0000000
+# define DEVICE_BASE 0xa0000000 
 #endif
+// 设备区域起始地址
 
 #define MMIO_BASE 0xa0000000
 
-#define SERIAL_PORT     (DEVICE_BASE + 0x00003f8)
-#define KBD_ADDR        (DEVICE_BASE + 0x0000060)
-#define RTC_ADDR        (DEVICE_BASE + 0x0000048)
-#define VGACTL_ADDR     (DEVICE_BASE + 0x0000100)
-#define AUDIO_ADDR      (DEVICE_BASE + 0x0000200)
-#define DISK_ADDR       (DEVICE_BASE + 0x0000300)
-#define FB_ADDR         (MMIO_BASE   + 0x1000000)
-#define AUDIO_SBUF_ADDR (MMIO_BASE   + 0x1200000)
+#define SERIAL_PORT     (DEVICE_BASE + 0x00003f8) // COM1
+#define KBD_ADDR        (DEVICE_BASE + 0x0000060) // KBD data port // 键盘数据端口
+#define RTC_ADDR        (DEVICE_BASE + 0x0000048) // RTC CMOS port 或许是这个
+#define VGACTL_ADDR     (DEVICE_BASE + 0x0000100) // VGA control port // 显卡控制端口
+#define AUDIO_ADDR      (DEVICE_BASE + 0x0000200) // Audio device port
+#define DISK_ADDR       (DEVICE_BASE + 0x0000300) // Disk device port
+#define FB_ADDR         (MMIO_BASE   + 0x1000000) // Framebuffer
+#define AUDIO_SBUF_ADDR (MMIO_BASE   + 0x1200000) // Audio buffer
 
 extern char _pmem_start;
 #define PMEM_SIZE (128 * 1024 * 1024)
--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,14 +5,18 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  // 读取 RTC 地址处的低 32 位和高 32 位时间戳（两个寄存器一共 64 位）
+  volatile uint32_t low_rtc = inl(RTC_ADDR + 0x00);
+  volatile uint32_t high_rtc = inl(RTC_ADDR + 0x04);
+  uptime->us = (uint64_t)high_rtc << 32 | low_rtc;
+  //uptime->us = 0;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
   rtc->second = 0;
-  rtc->minute = 0;
-  rtc->hour   = 0;
-  rtc->day    = 0;
-  rtc->month  = 0;
-  rtc->year   = 1900;
+  rtc->minute = 38;
+  rtc->hour   = 19;
+  rtc->day    = 25;
+  rtc->month  = 12;
+  rtc->year   = 1991;
 }

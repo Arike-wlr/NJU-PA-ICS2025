e02f623293e93ef8c51a2597e08c8dabee9a7337
Arike <1637928975@qq.com>
2025-10-19 19:46:16 +0800

    Finished studio...

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -15,19 +15,22 @@ int get_num_len(int num){
   return len;
 }
 
-void print_num2str(int num){
+int num2str(char *out, int num){
   int len=get_num_len(num);
   char num_char[12]; 
   if(num==0){
-    putch('0');
-    return;
+    *out = '0';
+    return len;
   }
   while (num) {
     num_char[--len] = (num % 10) + '0'; //取出最低位数字并转换为字符
     num /= 10; //去掉最低位数字
   }
-  for(int i=0; i<len; i++) //逐字符输出数字字符串
-    putch(num_char[i]); 
+  for(int i=0; i<len; i++) { //逐字符存储数字字符串
+    *out = num_char[i];
+    out++; 
+  }
+  return len;
 }
 
 int printf(const char *fmt, ...) {//printf是一个可变参数函数
@@ -55,7 +58,7 @@ int printf(const char *fmt, ...) {//printf是一个可变参数函数
 
 // 应该在 vsprintf 中实现核心格式化逻辑，返回写入的字符数
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  char *start = out; 
+  char *start = out; //记录输出字符串的起始位置
   //接下来开始遍历格式字符串fmt:
   for(const char *p = fmt; *p!='\0';p++){
     if(*p!='%'){ //格式内容，不是格式说明符，直接输出
@@ -69,29 +72,35 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
         case 'd':{ //整数,要把整数转换成字符串再输出
           int num = va_arg(ap, int); //获取下一个参数，期望类型是int
           if(num<0){ //处理负数
+            *out = '-';
+            out++;
             num = -num;
           }
-
+          int len = num2str(out, num); //把整数转换成字符串并存储到out
           break;
         }
         case 's':{ //字符串
           char *str = va_arg(ap, char*); //获取下一个参数，期望类型是char*
-          while(*str!='\0'){ //逐字符输出字符串
-
-            str++;
+          while(*str!='\0'){ 
+            *out = *str;
+            out++; str++;
           }
           break;
         }
         case 'c':{ //单个字符
           char ch = (char)va_arg(ap, int); //char在可变参数中提升为int
-
+          *out = ch;
+          out++;
           break;
         }
         case '%':{ //输出百分号本身
-
+          *out = '%';
+          out++;
           break;
         }
         default: //遇到未知的格式说明符，直接输出它们（包括%）
+          *out = '%'; out++; 
+          *out = *p; out++;
           break;
       }
     }

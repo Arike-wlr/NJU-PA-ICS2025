4d9ee62fd2b0b23a6ded96b7b58d648432812d9d
tracer-ics2025 <tracer@njuics.org>
2025-10-22 21:17:30 +0800

    >  compile NEMU 241300009 王琳睿 Linux LAPTOP-CHR6Q9L1 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:17:27 up 25 min,  1 user,  load average: 0.28, 0.08, 0.08

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,13 +1,12 @@
 #include <am.h>
 #include <nemu.h>
 
-#define KEYDOWN_MASK 0x8000 // 从键盘扫描码或按键状态值中提取按键按下/释放的信息
+#define KEYDOWN_MASK 0x8000 // 从键盘扫描码或按键状态值中提取按键按下/释放的信息,2进制的第15位为1表示按下,为0表示释放
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  int k = inb(KBD_ADDR);
+  int k = inw(KBD_ADDR);
   kbd->keydown = (k & KEYDOWN_MASK ? true : false);
   kbd->keycode = k & ~KEYDOWN_MASK;
 }
-//  kbd->keydown = false;
-//  kbd->keycode = AM_KEY_NONE;
+//  
 

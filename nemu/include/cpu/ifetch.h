3b46a79eb0e89f0bbcea98b993a08d465f99c78e
tracer-ics2025 <tracer@njuics.org>
2025-10-29 11:54:17 +0800

    >  compile NEMU 241300009 王琳睿 Linux LAPTOP-CHR6Q9L1 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:54:15 up 0 min,  1 user,  load average: 0.15, 0.03, 0.01

--- nemu/include/cpu/ifetch.h
+++ nemu/include/cpu/ifetch.h
@@ -16,11 +16,25 @@
 #ifndef __CPU_IFETCH_H__
 
 #include <memory/vaddr.h>
-
+/*
 static inline uint32_t inst_fetch(vaddr_t *pc, int len) {
   uint32_t inst = vaddr_ifetch(*pc, len);
   (*pc) += len;
   return inst;
+}*/
+static uint32_t inst_fetch(vaddr_t *pc, int len) {
+  uint32_t inst = vaddr_ifetch(*pc, len);
+  (*pc) += len;
+  return inst;
 }
-
+/*inline uint32_t inst_fetch(vaddr_t *pc, int len) {
+  uint32_t inst = vaddr_ifetch(*pc, len);
+  (*pc) += len;
+  return inst;
+}*/
+/*uint32_t inst_fetch(vaddr_t *pc, int len) {
+  uint32_t inst = vaddr_ifetch(*pc, len);
+  (*pc) += len;
+  return inst;
+}*/
 #endif

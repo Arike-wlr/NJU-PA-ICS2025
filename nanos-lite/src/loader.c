9bea3990796390827b0167b834d22fd81d4265a8
Arike <1637928975@qq.com>
2025-12-03 16:42:03 +0800

    tiaoshielf

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,6 +1,7 @@
 #include <proc.h> // 进程控制块（PCB）相关
 #include <elf.h>  // ELF 文件格式相关
 #include <klib.h> // 内存操作函数，如 memset
+#include <sys/types.h>
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
 # define Elf_Phdr Elf64_Phdr
@@ -17,19 +18,39 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   filename：要加载的 ELF 可执行文件的名称
   返回值：程序的入口地址
   */
+
   // read the ELF header
   Elf_Ehdr ehdr;
   ramdisk_read(&ehdr, 0, sizeof(ehdr));
+  printf("ELF header dump:\n");
+  printf("  e_ident[EI_MAG0-3]: %02x %02x %02x %02x\n",
+         ehdr.e_ident[0], ehdr.e_ident[1], ehdr.e_ident[2], ehdr.e_ident[3]);
+  printf("  e_type: %u\n", ehdr.e_type);
+  printf("  e_machine: %u\n", ehdr.e_machine);
+  printf("  e_version: %u\n", ehdr.e_version);
+  printf("  e_entry: 0x%x\n", ehdr.e_entry);
+  printf("  e_phoff: %u (should be 52)\n", ehdr.e_phoff);
+  printf("  e_shoff: %u (should be 36848)\n", ehdr.e_shoff);
+  printf("  e_ehsize: %u (should be 52)\n", ehdr.e_ehsize);
+  printf("  e_phentsize: %u (should be 32)\n", ehdr.e_phentsize);
+  printf("  e_phnum: %u (should be 4)\n", ehdr.e_phnum);
+  printf("  e_shentsize: %u\n", ehdr.e_shentsize);
+  printf("  e_shnum: %u\n", ehdr.e_shnum);
+
+  // verify ELF magic number
   if (ehdr.e_ident[EI_MAG0] != ELFMAG0 ||
       ehdr.e_ident[EI_MAG1] != ELFMAG1 ||
       ehdr.e_ident[EI_MAG2] != ELFMAG2 ||
       ehdr.e_ident[EI_MAG3] != ELFMAG3) {
     panic("Invalid ELF magic number");
   }
+
   // load each program segment
   for (int i = 0; i < ehdr.e_phnum; i++) {
     Elf_Phdr phdr;
-    ramdisk_read(&phdr, ehdr.e_phoff + i * sizeof(phdr), sizeof(phdr));
+    off_t offset = ehdr.e_phoff + i * sizeof(phdr);
+    Log("Reading program header %d at offset %d", i, offset);
+    ramdisk_read(&phdr,offset, sizeof(phdr));
 
     if (phdr.p_type == PT_LOAD) {
       // load segment from ramdisk to memory

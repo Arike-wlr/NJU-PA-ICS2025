8eea88fc0c88c33a234eaf48ce0fa8c7f567c605
tracer-ics2025 <tracer@njuics.org>
2025-12-03 23:00:55 +0800

    >  compile NEMU 241300009 王琳睿 Linux LAPTOP-CHR6Q9L1 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:00:54 up  7:16,  1 user,  load average: 0.25, 0.13, 0.07

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -18,50 +18,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   filename：要加载的 ELF 可执行文件的名称
   返回值：程序的入口地址
   */
-  struct __attribute__((packed)) PackedElfEhdr {
-    unsigned char e_ident[16];
-    uint16_t e_type;
-    uint16_t e_machine;
-    uint32_t e_version;
-    uint32_t e_entry;
-    uint32_t e_phoff;
-    uint32_t e_shoff;
-    uint32_t e_flags;
-    uint16_t e_ehsize;
-    uint16_t e_phentsize;
-    uint16_t e_phnum;
-    uint16_t e_shentsize;
-    uint16_t e_shnum;
-    uint16_t e_shstrndx;
-  };
-  // read the ELF header
-  struct PackedElfEhdr ehdr;
-  ramdisk_read(&ehdr, 0, sizeof(ehdr));
-
-  // verify ELF magic number
-  if (ehdr.e_ident[EI_MAG0] != ELFMAG0 ||
-      ehdr.e_ident[EI_MAG1] != ELFMAG1 ||
-      ehdr.e_ident[EI_MAG2] != ELFMAG2 ||
-      ehdr.e_ident[EI_MAG3] != ELFMAG3) {
-    panic("Invalid ELF magic number");
-  }
-
-  // load each program segment
-  for (int i = 0; i < ehdr.e_phnum; i++) {
-    Elf_Phdr phdr;
-    off_t offset = ehdr.e_phoff + i * sizeof(phdr);
-    Log("Reading program header %d at offset %d", i, offset);
-    ramdisk_read(&phdr,offset, sizeof(phdr));
-
-    if (phdr.p_type == PT_LOAD) {
-      // load segment from ramdisk to memory
-      ramdisk_read((void *)phdr.p_vaddr, phdr.p_offset, phdr.p_filesz);
-      // zero the memory region from p_filesz to p_memsz
-      memset((void *)(phdr.p_vaddr + phdr.p_filesz), 0, phdr.p_memsz - phdr.p_filesz);
-    }
-  }
-  // return the entry point of the program
-  return ehdr.e_entry;
+  return 0;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {

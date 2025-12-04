327c5c2bddf9747f75d51e18592fbaf40dfc14d9
tracer-ics2025 <tracer@njuics.org>
2025-12-04 13:12:23 +0800

    >  compile NEMU 241300009 王琳睿 Linux LAPTOP-CHR6Q9L1 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:12:15 up 31 min,  1 user,  load average: 0.34, 0.12, 0.11

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -47,6 +47,7 @@ Context* __am_irq_handle(Context *c) {
     //printf("mcause = %d", c->mcause);
     switch (c->mcause) {
       case -1: ev.event = EVENT_YIELD;c->mepc += 4; break;
+      case 1:case 4: ev.event = EVENT_SYSCALL;c->mepc += 4; break;
       default: ev.event = EVENT_ERROR; break;
     }
 

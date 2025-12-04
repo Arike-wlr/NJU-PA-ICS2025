0020a88a7a0199c28488c69baed1e92691752be2
tracer-ics2025 <tracer@njuics.org>
2025-12-04 23:23:03 +0800

    >  compile NEMU 241300009 王琳睿 Linux LAPTOP-CHR6Q9L1 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:23:02 up  1:52,  1 user,  load average: 0.24, 0.10, 0.04

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -77,28 +77,14 @@ int _write(int fd, void *buf, size_t count) {
 }
 
 void *_sbrk(intptr_t increment) {
-  static char buf[256];
-  int len;
-  
-  len = sprintf(buf, "_sbrk: increment=%ld, current break=%p\n", increment, (void*)program_break);
-  _write(1, buf, len);
-
   uintptr_t old_program_break=program_break;
   uintptr_t new_program_break=old_program_break+increment;
   
   int sys_ret=_syscall_(SYS_brk, new_program_break, 0, 0);
-  len = sprintf(buf, "_sbrk: current sys brk=%p\n", (void*)sys_ret);
-  _write(1, buf, len);
-
   if(sys_ret==0) {
     program_break=new_program_break;
-    len = sprintf(buf, "_sbrk success, returning %p\n", (void*)old_program_break);
-    _write(1, buf, len);
     return (void*)old_program_break;
   }
-
-  len = sprintf(buf, "_sbrk failed\n");
-    _write(1, buf, len);
   return (void*)-1;
 }
 

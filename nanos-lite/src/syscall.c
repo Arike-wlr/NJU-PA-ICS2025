d8881fca38f865985540f80f18ebed10237bb59d
tracer-ics2025 <tracer@njuics.org>
2025-12-04 10:45:46 +0800

    >  compile NEMU 241300009 王琳睿 Linux LAPTOP-CHR6Q9L1 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:45:42 up 37 min,  1 user,  load average: 0.54, 0.26, 0.13

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,17 +2,19 @@
 #include "syscall.h"
 
 void do_syscall(Context *c) {
-  Log("Handling syscall...");
+  
   uintptr_t a[4];
+  Log("Here? in do_syscall");
   a[0] = c->GPR1;
-
+  a[1] = c->GPR2;
+  Log("Syscall ID = %d", a[0]);
   switch (a[0]) {
     case SYS_yield:
       yield();
       c->GPRx = 0;
       break;
     case SYS_exit:
-      halt(c->GPR2);
+      halt(a[1]);
       c->GPRx = 0;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }

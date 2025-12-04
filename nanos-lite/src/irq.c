bf6205be17fa6e08c985e4863c22cb0b754368cd
tracer-ics2025 <tracer@njuics.org>
2025-12-04 10:37:48 +0800

    >  compile NEMU 241300009 王琳睿 Linux LAPTOP-CHR6Q9L1 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:37:44 up 29 min,  1 user,  load average: 0.49, 0.14, 0.04

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,12 +1,17 @@
 #include <common.h>
 
 //IRQ 的全称是 Interrupt ReQuest（中断请求）
+void do_syscall(Context *c);
 
 static Context* do_event(Event e, Context* c) {
   /*事件处理函数,目前:只处理未知事件（panic）*/
   switch (e.event) {
     case EVENT_YIELD:
     Log("EVENT_YIELD received");
+    break;
+    case EVENT_SYSCALL:
+    Log("EVENT_SYSCALL received");
+    do_syscall(c);
     break;
     default: panic("Unhandled event ID = %d", e.event);
   }

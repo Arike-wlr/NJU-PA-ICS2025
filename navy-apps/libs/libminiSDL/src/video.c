40ea5f68d7eb662f5638a68d1a7fc46055c7c859
tracer-ics2025 <tracer@njuics.org>
2025-12-05 18:38:09 +0800

    >  compile NEMU 241300009 王琳睿 Linux LAPTOP-CHR6Q9L1 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:38:08 up 6 min,  1 user,  load average: 0.10, 0.07, 0.02

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -33,14 +33,6 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
           ((uint32_t*)src->pixels)[row_off_s + sx + i];
       }
       else if(bits==8) {
-        /*
-        uint32_t color = (*src->format->palette).colors[((uint8_t*)src->pixels)[row_off_s + sx + i]].val;
-        if(color != 0){
-          printf("color %d,%d: idx=%d, c=%x\n", i, j, 
-            ((uint8_t*)src->pixels)[row_off_s + sx + i], color);
-        }
-
-        */
         ((uint8_t*)dst->pixels)[row_off_d + dx + i] = 
           ((uint8_t*)src->pixels)[row_off_s + sx + i];
       }
@@ -100,16 +92,13 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
     uint32_t pix[w+1];
     for (int j=0; j<h; j++) {
       int roff_s = j*s->w + x;
-      //int roff_d = j*w;
       for (int i=0; i<w; i++) {
         uint32_t color =  (*s->format->palette).colors[((uint8_t*)s->pixels)[roff_s+i]].val;
         color = ((color&0x00ff0000) >> 16) | (color&0x0000ff00) | ((color&0x000000ff) << 16);
-        //pix[roff_d+i] = color;
         pix[i] = color;
       }
       NDL_DrawRect(pix, x, j, w, 1);
     }
-    //NDL_DrawRect(pix, x, y, w, h);
   }
 }
 

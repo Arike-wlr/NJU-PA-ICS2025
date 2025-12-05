87341b5ee859146f02d9b06c03d81008b9c8b7ad
tracer-ics2025 <tracer@njuics.org>
2025-12-05 23:02:40 +0800

    >  compile NEMU 241300009 王琳睿 Linux LAPTOP-CHR6Q9L1 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:02:38 up 23 min,  1 user,  load average: 0.27, 0.21, 0.18

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,6 +5,7 @@ void do_syscall(Context *c);
 
 static Context* do_event(Event e, Context* c) {
   /*事件处理函数*/
+  Log("Handling event ID = %d", e.event);
   switch (e.event) {
     case EVENT_YIELD:
       Log("EVENT_YIELD received");

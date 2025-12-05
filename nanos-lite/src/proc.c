f6f54c57080fc3cde4195a15cafb39c46d136b75
tracer-ics2025 <tracer@njuics.org>
2025-12-05 18:34:29 +0800

    >  compile NEMU 241300009 王琳睿 Linux LAPTOP-CHR6Q9L1 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:34:28 up 3 min,  1 user,  load average: 0.21, 0.07, 0.02

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -31,7 +31,7 @@ void init_proc() {
   // naive_uload(NULL, "/bin/hello");
   // naive_uload(NULL,"/bin/file-test");
   // naive_uload(NULL,"/bin/timer-test");
-  naive_uload(NULL, "/bin/ nslider");
+  naive_uload(NULL, "/bin/nslider");
 }
 
 Context* schedule(Context *prev) {

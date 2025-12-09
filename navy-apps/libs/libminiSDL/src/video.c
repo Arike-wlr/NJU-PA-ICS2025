81fe9f1a7aed04bff0dea8d689b92fdcdbfbdfd4
tracer-ics2025 <tracer@njuics.org>
2025-12-09 14:12:14 +0800

    >  compile NEMU 241300009 王琳睿 Linux LAPTOP-CHR6Q9L1 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:12:12 up  1:46,  1 user,  load average: 0.46, 0.21, 0.12

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -193,11 +193,11 @@ void SDL_FreeSurface(SDL_Surface *s) {
 }
 
 SDL_Surface* SDL_SetVideoMode(int width, int height, int bpp, uint32_t flags) {
-  // if (flags & SDL_HWSURFACE) NDL_OpenCanvas(&width, &height);
+  int width0=width, height0=height;
+  if (flags & SDL_HWSURFACE) NDL_OpenCanvas(&width, &height);
 
-  printf("SetVideoMode: w=%d h=%d bpp=%d\n", width, height, bpp);
-  printf("SetVideoMode: return screen w=%d h=%d\n",width, height);
-  
+  printf("SetVideoMode: return screen w=%d h=%d\n",width0, height0);
+  width = width0; height = height0;
   return SDL_CreateRGBSurface(flags, width, height, bpp, DEFAULT_RMASK, DEFAULT_GMASK, DEFAULT_BMASK, DEFAULT_AMASK);
 }
 

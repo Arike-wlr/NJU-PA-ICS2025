810c156418fd8e8d7c70aca018d5bc481e2639b1
tracer-ics2025 <tracer@njuics.org>
2026-01-11 17:16:14 +0800

    >  compile NEMU 241300009 王琳睿 Linux LAPTOP-CHR6Q9L1 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:16:13 up 2 min,  1 user,  load average: 0.33, 0.08, 0.03

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -94,7 +94,7 @@ size_t context_uload(PCB *pcb, const char *filename, char *const argv[], char *c
 void init_proc() {
   // context_kload(&pcb[0], hello_fun, (void *)0);
   // context_kload(&pcb[1], hello_fun, (void *)1);
-  context_uload(&pcb[0], "/bin/nterm", (char*[]){NULL}, (char*[]){ NULL});
+  context_uload(&pcb[0], "/bin/exec-test", (char*[]){NULL}, (char*[]){ NULL});
   // Log("successfully load /bin/hello!!!");
   switch_boot_pcb();
 

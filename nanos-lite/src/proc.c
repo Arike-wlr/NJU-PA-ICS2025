903aeaf5d940121a8ac2aa26283183038b225b75
tracer-ics2025 <tracer@njuics.org>
2026-01-11 16:19:17 +0800

    >  compile NEMU 241300009 王琳睿 Linux LAPTOP-CHR6Q9L1 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:19:16 up 55 min,  1 user,  load average: 0.81, 0.41, 0.22

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -89,7 +89,7 @@ size_t context_uload(PCB *pcb, const char *filename, char *const argv[], char *c
 void init_proc() {
   // context_kload(&pcb[0], hello_fun, (void *)0);
   // context_kload(&pcb[1], hello_fun, (void *)1);
-  context_uload(&pcb[0], "/bin/nterm", (char*[]){NULL}, (char*[]){ NULL});
+  context_uload(&pcb[0], "/bin/hello", (char*[]){NULL}, (char*[]){ NULL});
   switch_boot_pcb();
 
   Log("Initializing processes...");
--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -4,7 +4,7 @@
 
 int main() {
   write(1, "Hello World!\n", 13);
-  assert(test2==0);
+  // assert(test2==0);
   int i = 2;
   volatile int j = 0;
   while (1) {

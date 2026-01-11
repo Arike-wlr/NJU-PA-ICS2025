843763788a16a9b6a5af898a1b15ec7c118293d6
tracer-ics2025 <tracer@njuics.org>
2026-01-11 16:54:14 +0800

    >  compile NEMU 241300009 王琳睿 Linux LAPTOP-CHR6Q9L1 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:54:12 up  1:31,  1 user,  load average: 0.20, 0.14, 0.10

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -70,14 +70,14 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 }
 
 Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
-  printf("ucontext called\n");
+  // printf("ucontext called\n");
   Context *c = (Context*)(kstack.end - sizeof(Context));
-  printf("Context at %p\n", c);
+  // printf("Context at %p\n", c);
   memset(c, 0, sizeof(Context));
   c->gpr[2] = (uintptr_t)(kstack.end - sizeof(Context)); // sp
-  printf("Set sp to %p\n", (void*)c->gpr[2]);
-  c->pdir = as->ptr;
-  printf("Set pdir to %p\n", c->pdir);
+  // printf("Set sp to %p\n", (void*)c->gpr[2]);
+  // c->pdir = as->ptr;
+  // printf("Set pdir to %p\n", c->pdir);
   c->mepc = (uintptr_t)entry;
   printf("Set mepc to %p\n", (void*)c->mepc);
   c->mstatus = 0x1800; // enable machine previous interrupt and set machine previous privilege mode to user mode
